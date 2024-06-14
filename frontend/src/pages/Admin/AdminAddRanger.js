a8639ef14b2e1c381618c6c8259bd560e491aa71
jagadish011 <munavallijagadish@gmail.com>
2024-06-14 14:36:39 +0530

    Login page

--- frontend/src/pages/Admin/AdminAddRanger.js
+++ frontend/src/pages/Admin/AdminAddRanger.js
@@ -380,7 +380,7 @@ const AddRanger = () => {
                 </div>
               )}
             </div>
-            <div className="w-full md:w-auto mt-4 md:mt-0 md:ml-6">
+            <div className="w-full md:w-auto mt-4 md:mt-0 ">
               <label
                 htmlFor="panImg"
                 className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -405,7 +405,7 @@ const AddRanger = () => {
           <div className="bottom-0 left-0 w-full flex justify-center p-4">
             <button
               type="submit"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-14 py-2.5 text-center"
             >
               Add Ranger
             </button>
