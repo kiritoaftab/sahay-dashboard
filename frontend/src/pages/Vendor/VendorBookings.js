c53b3363f3c60b6b0032a0c13ba007f763c294e1
dheeraj <dheerajrvithalkar@gmail.com>
2024-06-20 13:37:27 +0530

    contribution

--- frontend/src/pages/Vendor/VendorBookings.js
+++ frontend/src/pages/Vendor/VendorBookings.js
@@ -1,9 +1,157 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { BASE_URL } from "../../constants";
 
-const VendorBookings = () => {
+const VendorBooking = () => {
+  const [status, setStatus] = useState("");
+  const [bookingDoc, setBookingDoc] = useState("");
+
+  const getBookingsByStatus = async () => {
+    try {
+      const res = await axios.get(
+        `${BASE_URL}booking/getBookingByStatus/${status}`
+      );
+      console.log(res?.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getBookingsByStatus(status);
+  }, [status]);
+
+  const handleStatusChange = (newStatus) => {
+    setStatus(newStatus);
+  };
+
   return (
-    <div>VendorBookings</div>
-  )
-}
+    <>
+      <section className="w-screen md:w-full bg-background gap-4 flex flex-col p-5">
+        <h1 className="text-2xl font-medium">All Bookings</h1>
+        <div class="relative border border-gray-300 overflow-x-auto shadow-md sm:rounded-lg">
+          <div className="flex space-x-4 mb-4">
+            <button
+              onClick={() => handleStatusChange("INITIATED")}
+              className="btn">
+              Initiated({bookingDoc.length})
+            </button>
+            <button
+              onClick={() => handleStatusChange("COMPLETED")}
+              className="btn">
+              Completed ({bookingDoc.length})
+            </button>
+            <button
+              onClick={() => handleStatusChange("BOOKING_STARTED"||"PRICE_CALCULATED"||"PAYMENT_COMPLETED")}
+              className="btn">
+              On-going
+            </button>
+          </div>
+          <table class="w-full text-sm text-left rtl:text-right text-gray-500">
+            <thead class="text-xs text-gray-700 uppercase bg-slate-100 ">
+              <tr>
+                <th scope="col" class="p-4">
+                  <div class="flex items-center">
+                    <label for="checkbox-all-search" class="sr-only">
+                      checkbox
+                    </label>
+                  </div>
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Vendor name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Phone
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Service
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Booking Date-Time
+                </th>
+                {/* <th scope="col" class="px-6 py-3">
+                  Revenue
+                </th> */}
+                <th scope="col" class="px-6 py-3">
+                  Location
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Assign
+                </th>
+              </tr>
+            </thead>
+            {/* <tbody>
+              {vendorDoc && vendorDoc.length > 0 ? (
+                vendorDoc.map((vendor, index) => (
+                  <tr class="bg-white border-b hover:bg-gray-50">
+                    <td class="w-4 p-4">
+                      <div class="flex items-center">
+                        <label for="checkbox-all-search" class="sr-only">
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
+                      {vendor?.user?.userName}
+                    </th>
+                    <td class="px-6 py-4">{vendor?.user?.phone}</td>
+                    <td class="px-6 py-4">{vendor?.shopName}</td>
+                    <td class="px-6 py-4">{vendor?.noOfBooking}</td>
+                    <td class="px-6 py-4">{vendor?.noOfRanger}</td>
+                    <td class="px-6 py-4">
+                      <button
+                        className="bg-indigo-700 hover:bg-indigo-500 text-white text-xs font-normal p-1.5 rounded-md"
+                        onClick={() =>
+                          navigate(`/admin/editVendor/${vendor?._id}`)
+                        }>
+                        View More
+                      </button>
+                    </td>
+                    <td class="px-6 py-4">
+                      <button>
+                        <TrashIcon className="h-5 w-5 text-red-500" />
+                      </button>
+                      <button>
+                        <label class="inline-flex items-center cursor-pointer">
+                          <input
+                            type="checkbox"
+                            value=""
+                            class="sr-only peer"
+                          />
+                          <div class="relative w-11 h-6 bg-red-500  rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-green-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
+                        </label>
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>No Data about Vendors Available</tr>
+              )}
+            </tbody> */}
+          </table>
+          {/* <div className="mt-10 p-5 flex justify-center fixed bottom-0 left-0 w-full ">
+            <button
+              className={`focus:outline-none text-white bg-purple-500 hover:bg-purple-500 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 mb-2 mx-2`}
+              onClick={prevPage}
+              disabled={currentPage === 1}>
+              Previous
+            </button>
+            <span>
+              Showing {currentPage} of {totalPages}
+            </span>
+            <button
+              className={`focus:outline-none text-white bg-purple-500 hover:bg-purple-500 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2 mb-2 mx-2`}
+              onClick={nextPage}
+              disabled={currentPage === totalPages}>
+              Next
+            </button>
+          </div> */}
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default VendorBookings
+export default VendorBooking;
