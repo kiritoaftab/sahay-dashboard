63a8733476179600d0f81f807ab4dbd6d4079676
jagadish011 <munavallijagadish@gmail.com>
2024-06-27 13:04:32 +0530

    Changes in api and search parameter

--- frontend/src/pages/Admin/AdminRanger.js
+++ frontend/src/pages/Admin/AdminRanger.js
@@ -6,43 +6,31 @@ import axios from "../../axiosInstance/axiosApi";
 import { formatDuration } from "../../constants";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../constants";
-import { MdKeyboardArrowRight } from "react-icons/md";
-import { MdKeyboardArrowLeft } from "react-icons/md";
+import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
+import debounce from "lodash.debounce";
 
 const AdminRanger = () => {
   const [rangerList, setRangerList] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [serviceDetails, setServiceDetails] = useState({});
+  const [searchQuery, setSearchQuery] = useState("");
   const { auth } = useAuth();
   const navigate = useNavigate();
 
-  const fetchRangers = async () => {
+  const fetchRangers = async (query = "") => {
     try {
-      const res = await axios.get(
-        `${BASE_URL}ranger/getAllRanger?page=${currentPage}&pageSize=10`
-      );
-      setRangerList(res.data.rangerDoc);
-      console.log(res.data.rangerDoc);
-      setTotalPages(res.data.pagination.totalPages);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const fetchServices = async () => {
-    try {
-      const res = await axios.get(
-        `${BASE_URL}rangerService/populateServices?page=1&pageSize=10`
-      );
-
-      
-      const services = res.data.rangers.reduce((acc, ranger) => {
-        acc[ranger._id] = ranger.servicesDetails;
-        return acc;
-      }, {});
-      setServiceDetails(services);
-      console.log(services);
+      const url = query
+        ? `${BASE_URL}ranger/getByName/${query}`
+        : `${BASE_URL}rangerService/populateServices?page=${currentPage}&pageSize=10`;
+      const res = await axios.get(url);
+      console.log("API Response:", res.data); 
+      if (query) {
+        setRangerList(res.data.rangers);
+        setTotalPages(1);
+      } else {
+        setRangerList(res.data.rangers);
+        setTotalPages(res.data.pagination.totalPages);
+      }
     } catch (error) {
       console.error(error);
     }
@@ -50,8 +38,14 @@ const AdminRanger = () => {
 
   useEffect(() => {
     fetchRangers();
-    fetchServices();
   }, [currentPage]);
+
+  const handleSearchChange = debounce((event) => {
+    const query = event.target.value;
+    setSearchQuery(query);
+    setCurrentPage(1);
+    fetchRangers(query);
+  }, 300);
 
   const nextPage = () => {
     setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages));
@@ -62,73 +56,60 @@ const AdminRanger = () => {
   };
 
   return (
-    <section className="w-screen md:w-full h-full bg-background gap-4 flex flex-col">
-      <div className="w-screen md:w-full bg-background p-3 flex justify-between px-10">
-        <p className="text-2xl font-bold">Ranger Details</p>
-        <div className="relative inline-block text-left">
-          <div className="relative inline-block text-left">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="w-full p-2 pl-10 border rounded-md focus:outline-none"
-            />
-            <FaSearch
-              className="absolute right-4 top-1/2 transform -translate-y-1/2 text-gray-500"
-              size={20}
-            />
-          </div>
+    <section className="w-screen md:w-full lg:w-full h-full bg-background gap-4 flex flex-col">
+      <div className="w-full bg-background p-3 flex flex-col md:flex-row justify-between px-5 md:px-10">
+        <p className="text-xl md:text-2xl font-bold">Ranger Details</p>
+        <div className="relative mt-2 md:mt-0 w-full md:w-auto">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="w-full p-2 pl-10 border rounded-md focus:outline-none"
+            onChange={handleSearchChange}
+          />
+          <FaSearch
+            className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-500"
+            size={20}
+          />
         </div>
       </div>
-      <section className="w-screen md:w-full bg-background p-4">
+      <section className="w-full bg-background p-4 overflow-x-auto">
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Name
-              </th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Phone
-              </th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Services
-              </th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Booking
-              </th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Duration
-              </th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Details
-              </th>
-              <th className="px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center">
-                Action
-              </th>
+              {["Name", "Phone", "Services", "Booking", "Duration", "Details", "Action"].map((heading) => (
+                <th
+                  key={heading}
+                  className="px-2 md:px-6 py-3 text-xs font-medium text-gray-500 uppercase tracking-wider text-center"
+                >
+                  {heading}
+                </th>
+              ))}
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
             {rangerList.map((ranger, index) => (
               <tr key={index}>
-                <td className="px-6 py-4 whitespace-nowrap flex items-center justify-center">
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap flex items-center justify-center">
                   <span className="text-sm font-medium text-gray-900">
                     {ranger.firstName}
                   </span>
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
-                  {ranger?.user?.phone}
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
+                  {ranger?.userDetails?.phone}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-black text-center bg-[#FFB0153D] rounded-xl font-semibold">
-                  {serviceDetails[ranger._id]?.map((item) => (
-                    <div key={item._id}>{item.name}</div>
-                  ))}
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap text-sm text-black text-center bg-[#FFB0153D] rounded-xl font-semibold">
+                  {Array.isArray(ranger?.servicesDetails) ? 
+                    ranger.servicesDetails.map((service) => service.name).join(", ") : 
+                    ""
+                  }
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
                   {ranger.noOfBooking}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
-                  {formatDuration(ranger?.vendor?.amountEarned)}
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
+                  {formatDuration(ranger?.workedDuration)}
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-center">
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap text-sm text-center">
                   <button
                     onClick={() => navigate(`/admin/rangers/${ranger._id}`)}
                     className="text-white px-2 py-2 whitespace-nowrap text-sm rounded-xl bg-indigo-500"
@@ -136,7 +117,7 @@ const AdminRanger = () => {
                     View More
                   </button>
                 </td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-center">
+                <td className="px-2 md:px-6 py-4 whitespace-nowrap text-sm text-center">
                   <button>
                     <TrashIcon className="h-5 w-5 mr-2 text-red-500" />
                   </button>
@@ -147,7 +128,7 @@ const AdminRanger = () => {
                         value=""
                         className="sr-only peer"
                       />
-                      <div className="relative w-11 h-6 bg-red-500 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-green-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
+                      <div className="relative w-11 h-6 bg-red-500 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-green-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-green-600"></div>
                     </label>
                   </button>
                 </td>
@@ -155,25 +136,25 @@ const AdminRanger = () => {
             ))}
           </tbody>
         </table>
-        <div className="mt-10  flex  justify-center">
-          <div className=" border  bg-[#D9D9D9] rounded-full flex justify-center">
-          <button
-            className={`focus:outline-none text-black p-2   text-2xl`}
-            onClick={prevPage}
-            disabled={currentPage === 1}
-          >
-            <MdKeyboardArrowLeft />
-          </button>
-          <p className="p-2">
-             {currentPage} / {totalPages}
-          </p>
-          <button
-            className={`focus:outline-none text-black p-2  text-2xl`}
-            onClick={nextPage}
-            disabled={currentPage === totalPages}
-          >
-            <MdKeyboardArrowRight />
-          </button>
+        <div className="mt-10 flex justify-center">
+          <div className="border bg-[#D9D9D9] rounded-full flex justify-center">
+            <button
+              className={`focus:outline-none text-black p-2 text-2xl`}
+              onClick={prevPage}
+              disabled={currentPage === 1}
+            >
+              <MdKeyboardArrowLeft />
+            </button>
+            <p className="p-2">
+              {currentPage} / {totalPages}
+            </p>
+            <button
+              className={`focus:outline-none text-black p-2 text-2xl`}
+              onClick={nextPage}
+              disabled={currentPage === totalPages}
+            >
+              <MdKeyboardArrowRight />
+            </button>
           </div>
         </div>
       </section>
