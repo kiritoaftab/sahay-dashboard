f2544295380fcc753f90984ecfc66c97a0a7cfbd
aditya <adityakulkarni54321@gmail.com>
2024-09-28 12:56:02 +0530

    changes

--- frontend/src/pages/Vendor/VendorProfile.js
+++ frontend/src/pages/Vendor/VendorProfile.js
@@ -297,7 +297,7 @@ const VendorProfile = () => {
                 ) : (
                   <button
                     onClick={() => setIsEditing(true)}
-                    className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600"
+                    className="bg-blue-600 text-white px-4 py-2 rounded "
                   >
                     Edit
                   </button>
